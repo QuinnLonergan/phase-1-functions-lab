3e4186bb9fe15b8f3810c6d48863a88070702a5b
Quinn Lonergan <qui99lo@gmail.com>
2021-06-28 13:59:49 -0400

    commit tree cd73f620d82665d30dc4bc4b6453cf5011da2e4d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location < 42) {
+        return (location - 42)*-1;
+    } else {
+        return (location -42);
+    }
+}
+
+function distanceFromHqInFeet(location) {
+    let blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
+
+}
+
+function distanceTravelledInFeet(loc1, loc2) {
+    if (loc1 < loc2) return (loc2 - loc1)*264
+    else return (loc1 - loc2)*264
+}
+
+function calculatesFarePrice (loc1, loc2) {
+    let distance = distanceTravelledInFeet(loc1, loc2);
+    if (distance < 400) {
+        return 0
+    }else if (distance > 400 && distance < 2000) {
+        return (0.02 * distance) - 8
+    }else if (distance > 2000 && distance < 2500) {
+        return 25
+    }else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
